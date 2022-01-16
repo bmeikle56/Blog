9970b84d3db92eaa0a82a5351bf87133d0821805
Meikle, Braeden Andrew <bmeikle3@gatech.edu>
2022-01-16 16:18:04 -0500

    Add baseline files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is Braeden and Michael's App</h1>
+      <h1>455</h1>
+    </div>
+  );
+}
+
+export default App;
